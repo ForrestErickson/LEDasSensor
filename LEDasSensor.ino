84aa22ff53f48dc66be06eefb6dc4e88a84b291d
Forrest Erickson <forresterickson@comcast.net>
2021-05-05 14:46:21 -0400

    Set for one sensor and one LED. Reporting out serial port with heavy EMA. This produces a nice light detection.[F

--- LEDasSensor.ino
+++ LEDasSensor.ino
@@ -6,6 +6,14 @@
    In Ardunio, view serial ouput with Serial Ploter CTRL+SHIFT+L. 
    Ref: https://en.wikipedia.org/wiki/Moving_average#Cumulative_moving_average
 
+   Results:
+   Comment out all but the AnalogRead for Sensor 1 and the set LED1 code and set for ADC clock 1MHz
+   But leaving in the map function.
+   The onboard LED is pulsed around the AnalogRead reset to trigger an oscilliscope for measurement.
+   The frequency of the main loop is 9.7 to 10.13 KHz. 
+   By reducing the The output LED 
+   Adding the print statement back in loop reduces the sample rate to 2.5 to 2.9 KHz.
+
    Based on:
    LED light sensing
    Hunter Carlson
@@ -40,15 +48,22 @@ float val02EMA = 0;     // variable to store the exponential moving average sens
 
 //Threshold for dark indication.
 //Note there is no hysterisis on this comparision.
-//int light = 217;        // set light threshold
 //int light = 900;        // set in !AS lab with red LED as sensor before adding read to A0 normalization.
-int light = 50;        // set in !AS lab with red LED as sensor
-//int light = 10;        // set in !AS lab with red LED as sensor
+//int light = 217;        // set light threshold
+//int light = 150;        // set in !AS lab with red LED as sensor
+//int light = 50;         // set in !AS lab with red LED as sensor
+//int light = 35;         // set in !AS lab with red LED as sensor
+int light = 10;         // set in !AS lab with red LED as sensor
+//int light = 3;            // set in !AS lab with red LED as sensor
 
 //An exponential moving average (EMA),
 //float alpha = 0.99;
 //float alpha = 0.25;
-float alpha = 0.125;
+//float alpha = 0.125;
+//float alpha = 0.0625;
+//float alpha = 0.0375;     //P-P is 23 to 34 under lab lights.
+float alpha = 0.01;
+//float alpha = 0.001;        //P-P is 29 to 30 under lab lights.
 
 //Timer setup for ADC
 // defines for setting and clearing register bits
@@ -101,17 +116,15 @@ void loop()
   analogRead(senseReset);     //Read ground pin to normalize by discharge ADC. About 120uS for read.
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  //Toggle built in LED so we can sample rate.
   val01 = map(analogRead(sense01), 0 , 1024, 0, 5000);    // read sense01 led and map to mV.
-  analogRead(senseReset);     //Read ground pin to normalize by discharge ADC
-  val02 = map(analogRead(sense02), 0 , 1024, 0, 5000);    // read sense02 led
+//  analogRead(senseReset);     //Read ground pin to normalize by discharge ADC
+//  val02 = map(analogRead(sense02), 0 , 1024, 0, 5000);    // read sense02 led
 
   //Print out the EMA smoothed value.
   val01EMA = alpha * val01 + (1 - alpha) * val01EMA;
+//  val01EMA = val01;         // No EMA
 
   //Serial print adds significant delay. Even at 2Mbaud the sample rate is limited to: 1.6 to 2.0 KS/Sec
   Serial.println(val01EMA);     // 4.3KS/Sec when commented and with ADC clock 1MHz.
-
-  val02EMA = alpha * val02 + (1 - alpha) * val02EMA;
-//  Serial.println(val01EMA);
 
   //LED 1
 //  if (val01EMA >= light) {         
@@ -119,7 +132,10 @@ void loop()
 //  } else {
 //    digitalWrite(LED01, HIGH);    
 //  }
-  digitalWrite(LED01, (val01EMA <= light));
+  digitalWrite(LED01, !(val01EMA >= light));
+
+//  val02EMA = alpha * val02 + (1 - alpha) * val02EMA;
+//  Serial.println(val01EMA);
 
   //LED 2
   //About 26uS to write the LED
@@ -129,7 +145,7 @@ void loop()
 //    digitalWrite(LED02, HIGH);    
 //  }
 
-  digitalWrite(LED02, (val02EMA <= light));
+//  digitalWrite(LED02, (val02EMA <= light));
 
   //delay(1); //Slow down data.
 //  delayMicroseconds(SAMPLEPERIOD); //Slow down data. With out delay, and 1MHz clock sample freq 1.7KHz,
